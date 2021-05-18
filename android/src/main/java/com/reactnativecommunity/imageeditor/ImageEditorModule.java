fa593ddd3e841bd0262e2d140f6808a554e6d0a2
Mike Podgorniy <mike.podgorniy@gmail.com>
2021-05-18 10:27:02 +0300

    Could not determine MIME TYPE on Android 10 #73

--- android/src/main/java/com/reactnativecommunity/imageeditor/ImageEditorModule.java
+++ android/src/main/java/com/reactnativecommunity/imageeditor/ImageEditorModule.java
@@ -254,6 +254,22 @@ public class ImageEditorModule extends ReactContextBaseJavaModule {
       }
       return stream;
     }
+    
+    private String getMimeType() throws IOException {
+      BitmapFactory.Options outOptions = new BitmapFactory.Options();
+      outOptions.inJustDecodeBounds = true;
+
+      InputStream inputStream = openBitmapInputStream();
+
+      try {
+        BitmapFactory.decodeStream(inputStream, null, outOptions);
+        return outOptions.outMimeType;
+      } finally {
+        if (inputStream != null) {
+          inputStream.close();
+        }
+      }
+    }
 
     @Override
     protected void doInBackgroundGuarded(Void... params) {
@@ -272,7 +288,11 @@ public class ImageEditorModule extends ReactContextBaseJavaModule {
 
         String mimeType = outOptions.outMimeType;
         if (mimeType == null || mimeType.isEmpty()) {
-          throw new IOException("Could not determine MIME type");
+           mimeType = getMimeType();
+
+          if (mimeType == null || mimeType.isEmpty()) {
+            throw new IOException("Could not determine MIME type");
+          }
         }
 
         File tempFile = createTempFile(mContext, mimeType);
