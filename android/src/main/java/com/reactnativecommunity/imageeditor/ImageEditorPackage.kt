401a056b9c3e3f2f09586624971b6771562a79c1
Mateusz Mędrek <mateusz.medrek@callstack.com>
2023-11-14 23:10:35 +0100

    chore: lint Android code with spotless & ktfmt
    - add spotless & ktfmt
    - format existing android code
    - add lint & format scripts in package.json
    - add lint step in code-quality job

--- android/src/main/java/com/reactnativecommunity/imageeditor/ImageEditorPackage.kt
+++ android/src/main/java/com/reactnativecommunity/imageeditor/ImageEditorPackage.kt
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
  */
 package com.reactnativecommunity.imageeditor
 
@@ -24,21 +24,20 @@ class ImageEditorPackage : TurboReactPackage() {
     }
 
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-        val moduleList: Array<Class<out NativeModule?>> = arrayOf(
-            ImageEditorModule::class.java
-        )
+        val moduleList: Array<Class<out NativeModule?>> = arrayOf(ImageEditorModule::class.java)
         val reactModuleInfoMap: MutableMap<String, ReactModuleInfo> = HashMap()
         for (moduleClass in moduleList) {
             val reactModule = moduleClass.getAnnotation(ReactModule::class.java) ?: continue
-            reactModuleInfoMap[reactModule.name] = ReactModuleInfo(
-                reactModule.name,
-                moduleClass.name,
-                true,
-                reactModule.needsEagerInit,
-                reactModule.hasConstants,
-                reactModule.isCxxModule,
-                TurboModule::class.java.isAssignableFrom(moduleClass)
-            )
+            reactModuleInfoMap[reactModule.name] =
+                ReactModuleInfo(
+                    reactModule.name,
+                    moduleClass.name,
+                    true,
+                    reactModule.needsEagerInit,
+                    reactModule.hasConstants,
+                    reactModule.isCxxModule,
+                    TurboModule::class.java.isAssignableFrom(moduleClass)
+                )
         }
         return ReactModuleInfoProvider { reactModuleInfoMap }
     }
