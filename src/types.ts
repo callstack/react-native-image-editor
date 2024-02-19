d74d969f44153c358a49803acf85dfab54ab313e
D N <4661784+retyui@users.noreply.github.com>
2024-02-19 19:04:11 +0100

    feature: Use the default `resizeMode: cover` value across all platforms

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ type ImageCropDataFromSpec = Parameters<Spec['cropImage']>[1];
 export interface ImageCropData
   extends Omit<ImageCropDataFromSpec, 'resizeMode' | 'format'> {
   format?: 'png' | 'jpeg' | 'webp';
-  resizeMode?: 'contain' | 'cover' | 'stretch';
+  resizeMode?: 'contain' | 'cover' | 'stretch' | 'center';
   // ^^^ codegen doesn't support union types yet
   // so to provide more type safety we override the type here
 }
